e60c004cdcd7f0d6688c6610546063eeb11056b8
Alex Simkin <sashasimkin@gmail.com>
2016-01-07 23:58:51 +0200

    Update deploy.sh
    Lets show the commit after, instead of diffing before

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,9 @@ rm -rf *
 mv $TMPDIR/${TRAVIS_BUILD_ID}_built_site/* $TRAVIS_BUILD_DIR
 git config user.email "sashasimkin+travis@gmail.com"
 git config user.name "Travis CI"
-git diff
 git add . --all
 git commit -m "Generated from build: $TRAVIS_BUILD_ID triggered by commit $TRAVIS_COMMIT"
 git config --global push.default simple
-git status
+git show
 # git push origin master
 
