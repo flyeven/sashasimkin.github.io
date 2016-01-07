2555660d056e71e72f1ad35597ef7bb7a7db78ab
Alex Simkin <sashasimkin@gmail.com>
2016-01-07 19:30:53 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,18 @@ eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
 mv _site/ "$TMPDIR/${TRAVIS_BUILD_ID}_built_site"
-ls "$TMPDIR/${TRAVIS_BUILD_ID}_built_site"
-git fetch
+ls -la "$TMPDIR/${TRAVIS_BUILD_ID}_built_site"
+git fetch origin master:master
 git checkout master
 rm -rf *
 ls -la
-mv "$TMPDIR/${TRAVIS_BUILD_ID}_built_site/*" .
+ls -la "$TMPDIR/${TRAVIS_BUILD_ID}_built_site"
+mv "$TMPDIR/${TRAVIS_BUILD_ID}_built_site/*" "$TRAVIS_BUILD_DIR"
 git config user.email "sashasimkin+travis@gmail.com"
 git config user.name "Travis CI"
 ls -la
 # git add . --all
 # git commit -m "Generated from build: $TRAVIS_BUILD_ID triggered by commit $TRAVIS_COMMIT"
 # git config --global push.default simple
-# git push
+# git push origin master
 
