ff13a6e9eb92d3f1fc0f041603fc2f853237b1e1
Alex Simkin <sashasimkin@gmail.com>
2016-01-07 19:14:27 +0200

    Update deploy.sh
    Lets see what this beast does

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@ eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
 mv _site/ "$TMPDIR/${TRAVIS_BUILD_ID}_built_site"
-git checkout master
+git checkout origin/master
 rm -rf *
 mv "$TMPDIR/${TRAVIS_BUILD_ID}_built_site/*" .
-git add .
-git commit -m "Generated from build: $TRAVIS_BUILD_ID triggered by commit $TRAVIS_COMMIT"
-git push
+git config user.email "sashasimkin+travis@gmail.com"
+git config user.name "Travis CI"
+ls
+# git add . --all
+# git commit -m "Generated from build: $TRAVIS_BUILD_ID triggered by commit $TRAVIS_COMMIT"
+# git config --global push.default simple
+# git push
 
